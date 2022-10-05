8bf5e7a029cf5d6670a5fdc3916b30a2ebac4e77
kushalnigam0604 <nigamkushal0@gmail.com>
2022-10-06 02:45:19 +0530

    Add files

--- seeds/index.js
+++ seeds/index.js
@@ -25,7 +25,7 @@ const seedDB = async () => {
         const price = Math.floor(Math.random() * 20) + 10;
 
         const camp = new Campground({
-
+            author: '633de0f3466e77042061bdc8',
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
             image: 'https://source.unsplash.com/collection/483251',
